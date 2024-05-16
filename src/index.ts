b63afe9b4255bd553e4e1d76f5aaf8590d77ca4e
Alexandre Gressier <alexandre@gressier.dev>
2024-05-16 21:17:25 +0200

    Set up the UUID type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-function hello(): string {
-  return "Hello, world!"
+import { v4 as uuid } from "uuid"
+import { UUID } from "./types"
+
+class MyClass {
+  id: UUID
+
+  constructor() {
+    this.id = uuid()
+  }
+}
+
+function hello(): UUID {
+  return uuid()
 }
 
 console.log(hello())
